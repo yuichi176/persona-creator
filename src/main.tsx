9aecea92f9c9111f19fcc1126b327ad868799169
Yuichi Sugiyama <ozwald176@gmail.com>
2024-06-01 20:43:33 +0900

    Add CSS setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import 'the-new-css-reset/css/reset.css'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
